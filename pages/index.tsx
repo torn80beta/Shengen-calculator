745d9d89ad9e181a9a5364204ef049b88f1b5768
Alex <103459735+torn80beta@users.noreply.github.com>
2024-01-06 16:25:31 +0200

    date picker form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
+// import { Button } from "@/components/ui/button";
+import { DatePickerForm } from "@/components/ui/DatePickerForm/DatePickerForm";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { toast } = useToast();
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <p>Welcome to Shengen calculator!</p>
-    </main>
+    <>
+      <main
+        className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      >
+        <p>Welcome to Shengen calculator!</p>
+        <DatePickerForm />
+      </main>
+      <Toaster />
+    </>
   );
 }

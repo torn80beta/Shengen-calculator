9b0c53397a0b33cb1b028313b21e487322513591
Alex <103459735+torn80beta@users.noreply.github.com>
2024-01-08 14:10:07 +0200

    calc days

--- components/ui/DatePickerWithRange/DatePickerWithRange.tsx
+++ components/ui/DatePickerWithRange/DatePickerWithRange.tsx
@@ -35,25 +35,51 @@ export function DatePickerWithRange({
         setDateRange((prevDateRange) => [...prevDateRange, date]);
       }
     }
+    // console.log(date);
   }, [date, dateRange]);
 
   return (
     // console.log(dateRange),
     <div className={cn("grid gap-2", className)}>
       <div className="mb-24">
-        <p>Selected of ranges:</p>
-        <div>
+        <p className="mb-2">Selected of ranges:</p>
+        <ul className="flex flex-col gap-6">
           {dateRange?.map((date, index) => (
-            <div key={index} className="flex items-center justify-between">
-              <div>
+            <li
+              key={index}
+              className="flex flex-col items-left justify-between"
+            >
+              {/* <p>{Number(index) + 1}:</p> */}
+              <div className="flex gap-1">
+                <p>From: </p>
                 {date?.from ? format(date?.from, "LLL dd, y") : "Pick a date"}
               </div>
-              <div>
+              <div className="flex gap-1">
+                <p>To: </p>
                 {date?.to ? format(date?.to, "LLL dd, y") : "Pick a date"}
               </div>
-            </div>
+              <div className="flex flex-row mt-1 mb-3">
+                {/* <p>Milliseconds timestamp:</p>
+                <div>
+                  From: {date?.from ? format(date?.from, "T") : "Pick a date"}
+                </div>
+                <div>
+                  To: {date?.to ? format(date?.to, "T") : "Pick a date"}
+                </div> */}
+                <p>Number of selected days:</p>
+                <div>
+                  {date?.from && date?.to
+                    ? format(
+                        Number(format(date.to, "T")) -
+                          Number(format(date.from, "T")),
+                        "d"
+                      )
+                    : " "}
+                </div>
+              </div>
+            </li>
           ))}
-        </div>
+        </ul>
       </div>
       <Popover>
         <PopoverTrigger asChild>
@@ -91,6 +117,13 @@ export function DatePickerWithRange({
           />
         </PopoverContent>
       </Popover>
+      <Button
+        onClick={() => {
+          setDate(undefined), setDateRange([]);
+        }}
+      >
+        Clear all
+      </Button>
     </div>
   );
 }

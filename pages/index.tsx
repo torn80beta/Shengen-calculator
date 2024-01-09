fbc369be9f729268f77fd90b47e17e052b8d1c14
Alex <103459735+torn80beta@users.noreply.github.com>
2024-01-09 16:13:27 +0200

    cleanse

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/components/ui/use-toast";
-import { DatePickerForm } from "@/components/ui/DatePickerForm/DatePickerForm";
 import { DatePickerWithRange } from "@/components/ui/DatePickerWithRange/DatePickerWithRange";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,13 +10,9 @@ export default function Home() {
 
   return (
     <>
-      {/* <main
-        className={`page-container flex flex-col items-center w-full p-4 bg-gradient-to-br from-indigo-500 via-blue-500 to-violet-500 ${inter.className}`}
-      > */}
       <main
         className={`page-container w-full p-4 bg-gradient-to-br from-indigo-500 via-blue-500 to-violet-500 ${inter.className}`}
       >
-        {/* <p>Welcome to Shengen calculator!</p> */}
         <DatePickerWithRange />
       </main>
       <Toaster />
